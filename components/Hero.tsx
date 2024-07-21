3e0faed65713b7a2f2417466ca171bf457af66ee
devanshdp <devansh.pant@eka.care>
2024-07-21 19:00:57 +0530

    Hero style update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ import { FaLocationArrow } from "react-icons/fa";
 
 const Hero = () => {
   return (
-    <div className="pb-20 pt-36">
+    <div className="pb-20 pt-20">
       <div>
         <Spotlight
           className="-top-40 -left-40 md:-left-32 md:-top-20 h-screen"
